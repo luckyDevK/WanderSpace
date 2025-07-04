bb8132504ed744a4ff161d3e224568497445652b
LcDevJunior <veryluckykurniawan@gmail.com>
2025-07-04 17:20:14 +0700

    22222ss

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL!,
+    origin: 'https://localhost:5173',
     credentials: true,
   }),
 );
