9b892cffe752a37017540d5e2a8e90eb233deb46
LcDevJunior <veryluckykurniawan@gmail.com>
2025-07-04 14:28:50 +0700

    www

--- client/tests/integration/SearchBar.integration.test.tsx
+++ client/tests/integration/SearchBar.integration.test.tsx
@@ -1,13 +1,8 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
-import type { Mock } from 'vitest';
-
-import axios from 'axios';
 
 import SearchBar from '../../src/components/main/SearchBar';
-import { baseUrl } from '@/lib/api/fetchPlaces';
-import { searchPlace } from '@/lib/api/fetchPlaces';
 
 const mockHandleSearch = vi.fn();
 
@@ -32,21 +27,5 @@ describe('test search functionality', () => {
     await userEvent.type(input, 'borobudur');
 
     expect(mockHandleSearch).toHaveBeenCalledWith('borobudur');
-  });
-
-  test('display image after search', async () => {
-    const mockData22 = [
-      { id: 1, title: 'Beach', imageUrl: 'beach.jpg' },
-      { id: 2, title: 'Mountain', imageUrl: 'mountain.jpg' },
-    ];
-
-    (axios.get as Mock).mockResolvedValue({ data: mockData22 });
-
-    const res = await searchPlace('pwow');
-
-    expect(axios.get as Mock).toHaveBeenCalledWith(`${baseUrl}search?q=pwow`);
-
-    expect(res).toEqual(mockData22);
-    expect(axios.get).toHaveBeenCalledTimes(1);
   });
 });
